6acf0ffd62738cf59719c38ef5508d9568c01cff
YoshioIsmael <42271258+YoshioIsmael@users.noreply.github.com>
2019-01-10 19:27:37 -0600

    Add files via upload

--- WpfApplication1/WpfApplication1/Informacion/Unidad.cs
+++ WpfApplication1/WpfApplication1/Informacion/Unidad.cs
@@ -6,12 +6,7 @@ using System.Threading.Tasks;
 
 namespace WpfApplication1.Informacion
 {
-    public enum Accion
+    public class Unidad
     {
-        ATACAR_DIRECTO,
-        ATAQUE_INDIRECTO,
-        SEGREGAR,
-        RETIRADA,
-        MOVERCE
     }
 }
---
+++ WpfApplication1/WpfApplication1/Interfaces/IAccionAutonoma.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApplication1.Interfaces
+{
+    public interface IAccionAutonoma
+    {
+        /// <summary>
+        /// Método que permite ejecutar la acción autonoma
+        /// </summary>
+        void ejecutarAccionAutonomo();
+
+
+        
+    }
+}
